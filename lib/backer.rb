ee8e6e7acfb8482ec508f9fa6f2fc9c600410fec
Dametrik Fick <dametrik.fick@gmail.com>
2020-04-04 06:38:42 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,6 +8,6 @@ attr_reader :name, :backed_projects
   end
 
   def back_project(project)
-    @backed_projects << project || @backers << self
+    ? @backed_projects << project : @backers << self
   end
 end
